4bc0ad6197f64098bb46054453b46095a1577b52
Carlos Velasco <carlos@2activedesign.com>
2018-05-29 00:43:53 +0200

    clean

--- src/js/main/towers.js
+++ src/js/main/towers.js
@@ -103,7 +103,6 @@ function randomNumberRange(Min, Max) {
 
 function positionGenerator() {
   const x = randomNumberRange(-4, 4) * 10 + 5;
-
   const z = randomNumberRange(-4, 4) * 10 + 5;
   return { x, z };
 }
