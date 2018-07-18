56e9c7ab33941ef235f0d7e77930d623b73981db
Carlos Velasco <carlos@2activedesign.com>
2018-07-18 19:30:56 +0200

    copy serviceworker

--- node/cpDir.js
+++ node/cpDir.js
@@ -5,3 +5,7 @@ const version = process.env.npm_package_version;
 fsExtra.copy("./src/.htaccess", "./dist/.htaccess", err => {
   Error(err);
 });
+
+fsExtra.copy("./src/sw2.js", "./dist/sw2.js", err => {
+  Error(err);
+});
