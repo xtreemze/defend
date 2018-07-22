167d09a7ab6012f48b5640418b10923faf1cd9ed
Carlos Velasco <carlos@2activedesign.com>
2018-07-22 21:05:11 +0200

    lower volume for wave

--- src/js/main/sound.ts
+++ src/js/main/sound.ts
@@ -101,7 +101,7 @@ function enemyExplode(enemy: EnemySphere, level: number) {
 function newWave() {
   if (mapGlobals.soundOn) {
     FX.play({
-      volume: 18,
+      volume: 5,
       decay: 0.8,
       release: 0.9,
       frequency: 400,
