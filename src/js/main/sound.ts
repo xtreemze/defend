078f1d1d92f17072575b05fc47bd6ccbebebef13
Carlos Velasco <carlos@2activedesign.com>
2018-07-22 20:43:10 +0200

    louder wave

--- src/js/main/sound.ts
+++ src/js/main/sound.ts
@@ -101,7 +101,7 @@ function enemyExplode(enemy: EnemySphere, level: number) {
 function newWave() {
   if (mapGlobals.soundOn) {
     FX.play({
-      volume: 24,
+      volume: 28,
       decay: 0.8,
       release: 0.9,
       frequency: 70,
