6eb5ec2b244c4e94f1d7d954e474997068738902
Carlos Velasco <carlos.velasco@lequa.com>
2018-07-24 16:42:59 +0200

    analytics added

--- src/sw2.js
+++ src/sw2.js
@@ -1,4 +1,15 @@
 // Install stage sets up the index page (home page) in the cache and opens a new cache
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+  workbox.routing.registerRoute(
+    new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
+    workbox.strategies.cacheFirst({
+      cacheName: "google-fonts",
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        })
+      ]
+    })
+  );
 });
