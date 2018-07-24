752b2c34d17f4813c01d6b8718858b2406ae9681
Carlos Velasco <carlos.velasco@lequa.com>
2018-07-24 16:44:39 +0200

    offline analytics

--- src/sw2.js
+++ src/sw2.js
@@ -1,6 +1,7 @@
 // Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener("install", event => {
   workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+  workbox.googleAnalytics.initialize();
   workbox.routing.registerRoute(
     new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
     workbox.strategies.cacheFirst({
