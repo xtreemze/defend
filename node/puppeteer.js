fac5b1b2d08c342b7aa884aff6f403c1aeb42865
Velasco Carlos <carlos.velasco@afconsult.com>
2019-04-25 12:47:00 +0200

    new screenshot

--- node/puppeteer.js
+++ node/puppeteer.js
@@ -1,44 +1,44 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer
-    .launch({
-      headless: false,
-      timeout: 2000
-    })
-    .catch(err => Error(err));
+	const browser = await puppeteer
+		.launch({
+			headless: false,
+			timeout: 2000
+		})
+		.catch(err => Error(err));
 
-  const page = await browser.newPage().catch(err => Error(err));
+	const page = await browser.newPage().catch(err => Error(err));
 
-  await page.setViewport({ width: 800, height: 500 }).catch(err => Error(err));
+	await page.setViewport({ width: 1024, height: 1024 }).catch(err => Error(err));
 
-  await page
-    .goto("http://localhost:8080/", {
-      waitUntil: "networkidle2",
-      timeout: 5000
-    })
-    .catch(err => Error(err));
+	await page
+		.goto("http://localhost:8080/", {
+			waitUntil: "networkidle2",
+			timeout: 5000
+		})
+		.catch(err => Error(err));
 
-  await setTimeout(async () => {
-    await page
-      .screenshot({
-        path: "./release/screenshot.png"
-      })
-      .catch(err => Error(err));
-    await setTimeout(async () => {
-      await page
-        .screenshot({
-          path: "./release/screenshot2.png"
-        })
-        .catch(err => Error(err));
-      await setTimeout(async () => {
-        await page
-          .screenshot({
-            path: "./release/screenshot3.png"
-          })
-          .catch(err => Error(err));
-        await browser.close();
-      }, 4800);
-    }, 6400);
-  }, 15400);
+	await setTimeout(async () => {
+		await page
+			.screenshot({
+				path: "./release/screenshot.png"
+			})
+			.catch(err => Error(err));
+		await setTimeout(async () => {
+			await page
+				.screenshot({
+					path: "./release/screenshot2.png"
+				})
+				.catch(err => Error(err));
+			await setTimeout(async () => {
+				await page
+					.screenshot({
+						path: "./release/screenshot3.png"
+					})
+					.catch(err => Error(err));
+				await browser.close();
+			}, 4800);
+		}, 6400);
+	}, 15400);
 })();
---
+++ release/pwa/Defend/Polyfills/cordova/platforms/windows/WindowsCordova-next-steps.md
@@ -0,0 +1,25 @@
+﻿# Test - Build - Submit
+
+## Test
+
+1. Your application uses the same rendering and JavaScript Engine as the Microsoft Edge Browser. Most of you testing can be done on your website and in the Edge browser on Windows 10.
+
+2. To test your application on a device, download the PWA Builder test harness from the Store, and follow the directions. _Coming soon!_
+
+> **Note:** Looking for some debugging tools that work on all your platforms? Try [Vorlon.js](http://www.vorlonjs.com/). It makes mobile testing a breeze, and works inside the app PWA Builder apps.
+
+## Build
+
+1. Download and install Visual Studio (community edition works fine) and open the source code folder.
+
+2. Open the Project in Visual Studio.
+
+3. Use the store commands from Visual Studio to create the app package.
+
+## Submit to Store
+
+1. Set up a Microsoft Developer account [here](http://dev.windows.com/).
+
+2. Reserve the name of your app.
+
+3. Upload your appx package.
