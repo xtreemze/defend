24ef34172941a835acd7192da688c59383d56081
Carlos Velasco <xtreemze@gmail.com>
2026-09-25 14:25:26 +0800

    Disable Chromium throttling for showcase capture

--- experiments/hybrid-engine/showcase/playwright.showcase.config.mjs
+++ experiments/hybrid-engine/showcase/playwright.showcase.config.mjs
@@ -4,6 +4,17 @@ export default {
   testDir: "./showcase",
   testMatch: "capture.mjs",
   outputDir: "./artifacts/e2e-media/playwright",
+  launchOptions: {
+    headless: true,
+    args: [
+      "--autoplay-policy=no-user-gesture-required",
+      "--disable-background-timer-throttling",
+      "--disable-renderer-backgrounding",
+      "--disable-backgrounding-occluded-windows",
+      "--disable-frame-rate-limit",
+      "--disable-gpu-vsync",
+    ],
+  },
   projects: showcase.projects.map((project) => ({
     name: project.name,
     metadata: { showcase: project },
@@ -12,7 +23,7 @@ export default {
       viewport: project.viewport,
       hasTouch: project.device.hasTouch,
       isMobile: project.device.isMobile,
-      video: "on",
+      video: "off",
       screenshot: "on",
     },
   })),
